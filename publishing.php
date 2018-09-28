04d93e325fb05f1ec59bc4ff279e69c04085c499
vlucis <v-lucis@microsoft.com>
2018-09-28 08:50:33 -0300

    Update publishing.php

--- publishing.php
+++ publishing.php
@@ -1,15 +1,51 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <title>PokeFix - V 1.0</title>
+<body>
+        <title>PokeFix - V 1.0</title>
     <link rel="shortcut icon" type="image/png" href="img/PokeIco.png"/>
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <link rel="stylesheet" type="text/css" href="assets/css/animation.css">
     <link rel="stylesheet" href="assets/font-awesome/font-awesome-4.7.0/css/font-awesome.min.css">
+    
+<?php
+function file_get_contents_curl($url)
+{
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_HEADER, 0);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+    $data = curl_exec($ch);
+    curl_close($ch);
+
+    return $data;
+}
+
+$url=$_POST['url'];
 
 
-</head>
-<body>
+$html = file_get_contents_curl("$url");
+
+//parsing begins here:
+$doc = new DOMDocument();
+@$doc->loadHTML($html);
+$nodes = $doc->getElementsByTagName('title');
+
+//get and display what you need:
+$title = $nodes->item(0)->nodeValue;
+
+$metas = $doc->getElementsByTagName('meta');
+
+for ($i = 0; $i < $metas->length; $i++)
+{
+    $meta = $metas->item($i);
+    if($meta->getAttribute('name') == 'description')
+        $description = $meta->getAttribute('content');
+    if($meta->getAttribute('name') == 'updated_at')
+        $lastUpdate = $meta->getAttribute('content');
+}
+?>
 
 <div class = "main">    <div class = "wrapper">
     <div class="pokefix-container">
@@ -21,7 +57,7 @@
         </div>
         <div class="input-container">
 
-        <form action="https://github.com/vlucis/pokefix/blob/master/publishing.php" method="POST">
+        <form action="publishing.php" method="POST">
             <p>URL(Single):</p>
             <input id="input-url" name="url" value="<?php echo htmlspecialchars($url); ?>">
             
